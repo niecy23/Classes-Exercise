787dad34e9573809c106fafe446a0e092744c779
Chanise Hart <niecy23@gmail.com>
2022-12-22 23:14:06 -0700

    Exercise 1 Before the Video

---
+++ Classes/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+
+		public string Make { get; set; }
+
+		public string Model { get; set; }
+
+		public int Year { get; set; }
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Setting values for a new instance of the car class and printing them to the console. 
+            var newCar = new Car();
+
+            newCar.Make = "Honda";
+
+            newCar.Model = "Civic";
+
+            newCar.Year = 2000;
+
+            Console.WriteLine($"My first car was a {newCar.Year} {newCar.Make} {newCar.Model}."); 
         }
     }
 }
